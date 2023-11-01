b941042887b40deae6635f634b90a76267139763
Jorge <jorge.guerrero@sitel.com>
2023-11-01 08:15:24 -0500

    final changes to first exercise

--- EulerProject/Algorithms/1.cs
+++ EulerProject/Algorithms/1.cs
@@ -9,5 +9,15 @@ namespace EulerProject.Algorithms
     internal class _1
     {
 
+        //Find the sum of all the multiples of 3 or 5 below the provided parameter value number.
+        public static long multiplesOf3and5(int num)
+        {
+            long sum = 0;
+            for (int i = 0; i < num; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0) sum += i;
+            }
+            return sum;
+        }
     }
 }
--- EulerProject/Program.cs
+++ EulerProject/Program.cs
@@ -1,2 +1,4 @@
-﻿Console.WriteLine("Running exercise");
+﻿using EulerProject.Algorithms;
+Console.WriteLine("Running exercise");
 
+Console.WriteLine($"the result is: {_1.multiplesOf3and5(1000)}");
