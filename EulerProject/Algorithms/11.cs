cc855a7a19c8f76982c98127fc937104e0f734b4
Jorge <jorge.guerrero@sitel.com>
2023-11-10 16:15:00 -0500

    stagging changes

--- EulerProject/Algorithms/11.cs
+++ EulerProject/Algorithms/11.cs
@@ -9,48 +9,46 @@ namespace EulerProject.Algorithms
 {
     public class _11
     {
-        public static long largestGridProduct(int[,] gird)
+        public static long largestGridProduct(int[,] grid)
         {
             long maxResult = 0;
-            int axln = gird.GetLength(0);
-            int axh = gird.GetLength(0);
             //from left to right
 
             //this loop iterates between y axis
-            for (int y = 0; y < (gird.GetLength(1) - 1); y++)
+            for (int y = 0; y < (grid.GetLength(1) - 1); y++)
             {
                 //this moves the pointer one to the right at the time
-                for (int i = 0; i < (gird.GetLength(0) - 4); i++)
+                for (int i = 0; i < (grid.GetLength(0) - 4); i++)
                 {
-                    long sum = gird[i, y] * gird[(i + 1), y] * gird[(i + 2), y] * gird[(i + 3), y];
+                    long sum = grid[i, y] * grid[(i + 1), y] * grid[(i + 2), y] * grid[(i + 3), y];
                     if (sum > maxResult) maxResult = sum;
                 }
             }
             //from the top to the bottom (the one before with inverted axis)
-            for (int y = 0; y < (gird.GetLength(0) - 1); y++)
+            for (int y = 0; y < (grid.GetLength(0) - 1); y++)
             {
-                for (int i = 0; i < (gird.GetLength(1) - 4); i++)
+                for (int i = 0; i < (grid.GetLength(1) - 4); i++)
                 {
-                    long sum = gird[y, i] * gird[y, (i + 1)] * gird[y, (i + 2)] * gird[y, (i + 3)];
+                    long sum = grid[y, i] * grid[y, (i + 1)] * grid[y, (i + 2)] * grid[y, (i + 3)];
                     if (sum > maxResult) maxResult = sum;
                 }
             }
             //diagonal aggregation down right
-            for (int y = 0; y < (gird.GetLength(1) - 4); y++)
+            for (int y = 0; y < (grid.GetLength(1) - 4); y++)
             {
-                for (int i = 0; i < (gird.GetLength(0) - 4); i++)
+                for (int i = 0; i < (grid.GetLength(0) - 4); i++)
                 {
-                    long sum = gird[i, y] * gird[(i + 1), (y + 1)] * gird[(i + 2), (y + 2)] * gird[(i + 3), (y + 3)];
+                    long sum = grid[i, y] * grid[(i + 1), (y + 1)] * grid[(i + 2), (y + 2)] * grid[(i + 3), (y + 3)];
                     if (sum > maxResult) maxResult = sum;
                 }
             }
 
             //diagonal aggregation down left
-            for (int y = 0; y < (gird.GetLength(1) - 3); y++)
+            for (int y = 0; y < (grid.GetLength(1) - 3); y++)
             {
-                for (int i = (gird.GetLength(0) - 1); i > 3; i--)
+                for (int i = (grid.GetLength(0) - 1); i > 3; i--)
                 {
-                    long sum = gird[i, y] * gird[(i - 1), (y + 1)] * gird[(i - 2), (y + 2)] * gird[(i - 3), (y + 3)];
+                    long sum = grid[i, y] * grid[(i - 1), (y + 1)] * grid[(i - 2), (y + 2)] * grid[(i - 3), (y + 3)];
                     if (sum > maxResult) maxResult = sum;
                 }
             }
---
+++ EulerProject/Algorithms/12.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerProject.Algorithms
+{
+    public class _12
+    {
+        public static int divisibleTriangleNumber(int n)
+        {
+            int i = 1, result = 0;
+            bool found = false;
+
+            while (!found)
+            {
+                int triangleNumber = getTriangleNumber(i);
+                if (getNumerOfDivisors(triangleNumber) >= n)
+                {
+                    result = triangleNumber;
+                    found = true;
+                }
+                else i++;
+            }
+            return result;
+        }
+        private static int getTriangleNumber(int n)
+        {
+            return ((int)Math.Pow(n, 2) + n) / 2;
+        }
+        private static int getNumerOfDivisors(int n)
+        {
+            int count = 0;
+            // Divisors come in pairs
+            for (int i = 1; i < Math.Sqrt(n); i++)
+                if (n % i == 0) count += 2;
+            // Except for the sqrt
+            if (n % Math.Sqrt(n) == 0) count += 1;
+
+            return count;
+        }
+    }
+}
