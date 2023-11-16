51a0d45075ef3e3cefa4ef55507b470341a17dc0
Jorge <jorge.guerrero@sitel.com>
2023-11-16 11:03:45 -0500

    seventeenth exercise completed

--- EulerProject/Algorithms/17.cs
+++ EulerProject/Algorithms/17.cs
@@ -12,14 +12,57 @@ namespace EulerProject.Algorithms
         public static int numberLetterCounts(int limit)
         {
             int totalLetters = 0;
-            for (int i = 0; i < limit; i++)
+            for (int i = 1; i <= limit; i++)
             {
-                string number = HumanFriendlyInteger.IntegerToWritten(i);
-                number = String.Join("", number.Split(' '));
-
+                string number = numberToLetters(i);
                 totalLetters += number.Length;
             }
             return totalLetters;
+        }
+
+        public static string numberToLetters(int number)
+        {
+            string strNumber = "";
+            string[] oneToNineteen = { "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+            string[] tens = { "", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+
+            if (number == 0)
+            {
+                strNumber = "zero";
+            }
+            else if (number < 20)
+            {
+                strNumber += oneToNineteen[number];
+            }
+            else if (number < 100)
+            {
+                strNumber += tens[number / 10];
+                strNumber += oneToNineteen[number % 10];
+            }
+            else if (number < 1000 && number % 100 == 0)
+            {
+                strNumber += oneToNineteen[number / 100] + "Hundred";
+            }
+            else if (number < 1000)
+            {
+                strNumber += oneToNineteen[number / 100];
+                strNumber += "hundredand";
+                if (number % 100 < 20)
+                {
+                    strNumber += oneToNineteen[number % 100];
+                }
+                else
+                {
+                    strNumber += tens[(number % 100) / 10];
+                    strNumber += oneToNineteen[number % 10];
+                }
+
+            }
+            else if (number >= 1000)
+            {
+                strNumber = oneToNineteen[number / 1000] + "thousand";
+            }
+            return strNumber;
         }
     }
 }
